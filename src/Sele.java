b1645ea0a7c60779af5c47c21e89e1ae13350e75
Tom <tbek1111@yahoo.com>
2017-06-25 18:15:00 -0600

    Chrome Works

--- src/Sele.java
+++ src/Sele.java
@@ -1,16 +1,20 @@
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
-public class GoogleTest {
+public class Sele {
   @Test
   public void userCanSearch() {
-    GooglePage page = open("http://google.com/ncr", GooglePage.class);
-    SearchResultsPage results = page.searchFor("selenide");
-    results.getResults().shouldHave(size(10));
-    results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
+	  System.setProperty("webdriver.chrome.driver",
+		           "/Users/Tom/Documents/workspace/WebDrive/repo/PlayingWithSelenide/Resources/chromedriver");
+	  System.setProperty("selenide.browser", "chrome");
+	  open("http://google.com/ncr");
+	    //$(By.name("q")).val("selenide").pressEnter();
   }
+  
 }
